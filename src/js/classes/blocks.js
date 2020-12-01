6e3e5fcd3be8131659e106d1eeb27bd074f7b6fd
Nikita Belevich <belevish_1998@mail.ru>
2020-12-01 16:45:30 +0300

    refactor: Для каждого блока сайта создано представление класса
    Для каждой части сайта, т.е для блоков созданы свои именованные классы, по типу как это представленно в DOM: HTMLInputElement, HTMLDivElement. Они наследуют от класса Block, вызывают его конструктор с уже определённым типом, а в себя параметрами принимают только значение value и объект с опциями.

--- src/js/classes/blocks.js
+++ src/js/classes/blocks.js
@@ -8,4 +8,23 @@ export class Block {
     }
 }
 
-
+export class TitleBlock extends Block {
+    constructor(value, options) {
+        super('title', value, options);
+    }
+}
+export class ImageBlock extends Block {
+    constructor(value, options) {
+        super('image', value, options);
+    }
+}
+export class TextBlock extends Block {
+    constructor(value, options) {
+        super('text', value, options);
+    }
+}
+export class ColumnsBlock extends Block {
+    constructor(value, options) {
+        super('columns', value, options);
+    }
+}
--- src/js/modules/_model.js
+++ src/js/modules/_model.js
@@ -1,23 +1,28 @@
 'use strict';
 
 import img1 from '../../img/parcel.png'
-import { Block } from '../classes/blocks';
+import {
+    TitleBlock,
+    TextBlock,
+    ImageBlock,
+    ColumnsBlock,
+} from '../classes/blocks';
 
 export const model = [
-    new Block('title', 'Конструктор сайта (компонентный подход на чистом JS)', {
+    new TitleBlock('Конструктор сайта (компонентный подход на чистом JS)', {
         tag: 'h2',
         styles: 'header-title-row',
     }),
-    new Block('image', img1, {
+    new ImageBlock(img1, {
         imageStyles: 'parcel-img',
         parentColPadding: 'pl-0 pr-0',
         alt: 'Parcel'
     }),
-    new Block('text', 'Компонентный подход к разработке с использованием Parcel.', {
+    new TextBlock('Компонентный подход к разработке с использованием Parcel.', {
         tag: 'p',
         styles: 'project-description',
     }),
-    new Block('columns', [
+    new ColumnsBlock([
         'Column 1',
         'Column 2',
         'Column 3',
@@ -26,6 +31,5 @@ export const model = [
         tag: 'p',
         styles: 'product-col',
     }),
-    
     
 ];
