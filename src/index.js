e5c025b901d9aa08d540f94dec332f89cb17d0b9
Nikita Belevich <belevish_1998@mail.ru>
2020-12-01 17:36:51 +0300

    refactor: Функции генерации блока перенесены в методы классов
    Функции, которые генерируют HTML строку на основе данных из классов блоков теперь перемещены в классы этих элементов в метод toHTML, файл _creators.js удалён, эти функции теперь хранятся каждая в своём классе.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import './scss/main-style.scss'; // SCSS
 
 
 import {model} from './js/modules/_model';
-import {creators} from './js/modules/_creators';
 
 
 const $site = document.querySelector('#site');
 model.forEach(block => {
-    
-    const creatorHTML = creators[block.type];
-    if (creatorHTML) {
-       $site.insertAdjacentHTML('beforeend', creatorHTML(block)); 
-    }
-    
+    $site.insertAdjacentHTML('beforeend', block.toHTML());
 });
 
--- src/js/classes/blocks.js
+++ src/js/classes/blocks.js
@@ -1,10 +1,16 @@
 'use strict';
 
+import {col, row} from '../modules/_utils';
+
 export class Block {
     constructor(type, value, options) {
         this.type = type;
         this.value = value;
         this.options = options;
+    }
+
+    toHTML() {
+        throw new Error('Метод toHTML должен быть реализован!');
     }
 }
 
@@ -12,19 +18,36 @@ export class TitleBlock extends Block {
     constructor(value, options) {
         super('title', value, options);
     }
+    toHTML() {
+        const {tag = 'h1', styles} = this.options;
+        return row(col(`<${tag}>${this.value}</${tag}>`), styles);
+    }
 }
 export class ImageBlock extends Block {
     constructor(value, options) {
         super('image', value, options);
+    }
+    toHTML() {
+        const {alt, imageStyles, parentColPadding} = this.options;
+        return row(col(`<img src="${this.value}" alt="${alt}" class="${imageStyles}">`, parentColPadding));
     }
 }
 export class TextBlock extends Block {
     constructor(value, options) {
         super('text', value, options);
     }
+    toHTML() {
+        const {tag = 'p', styles} = this.options;
+        return row(col(`<${tag}>${this.value}</${tag}>`), styles);
+    }
 }
 export class ColumnsBlock extends Block {
     constructor(value, options) {
         super('columns', value, options);
+    }
+    toHTML() {
+        const {tag = 'p', styles} = this.options;
+        const columns = this.value.map(elem => col(`<${tag}>${elem}</${tag}>`, styles));
+        return row(columns.join(''));
     }
 }
